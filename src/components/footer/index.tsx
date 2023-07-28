5f9a86a47a0257c80c47bcd151db4ce9db61c0eb
Kauan C. S <kauan13cs@gmail.com>
2023-07-28 15:02:53 -0300

    Adding modal rules

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,10 +1,29 @@
+import { useState } from 'react'
+import Rules from '../../assets/image-rules.svg'
+import CloseIcon from '../../assets/icon-close.svg'
+
 const Footer = () => {
+  const [isModalOpen, setIsModalOpen] = useState(true)
+
   return (
-    <div className='flex justify-end p-4'>
-      <button className='p-2 border-2 w-28 rounded-xl border-slate-500'>
-        RULES
-      </button>
-    </div>
+    <>
+      {isModalOpen && 
+        <div className='absolute top-0 flex items-center justify-center w-full h-full bg-black/50' >
+          <div className='container p-8 bg-white rounded h-fit w-fit'>
+            <div className='flex justify-between'>
+              <h1 className='mb-8 text-xl font-extrabold text-black'>RULES</h1>
+              <img className='w-6 h-6 cursor-pointer' src={CloseIcon} onClick={() => setIsModalOpen(false)} />
+            </div>
+            <img src={Rules} />
+          </div>
+        </div>
+      }
+      <div className='flex justify-end p-4'>
+        <button onClick={() => setIsModalOpen(true)} className='p-2 border-2 w-28 rounded-xl border-slate-500'>
+          RULES
+        </button>
+      </div>
+    </>
   )
 }
 
